984d1c7c4af377f473b5fcb4c9fc95977b1d3309
qzy <weelgod@gmail.com>
2018-09-17 20:35:10 +0800

    8-14

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Fib from "./Fib.js";
+import OtherPage from "./OtherPage.js";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherPage">OtherPage</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherPage" component={OtherPage} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
 import axios from "axios";
+import { Component } from "react";
 
 class Fib extends Component {
   state = {
@@ -15,10 +15,57 @@ class Fib extends Component {
 
   async fetechValues() {
     const value = await axios.get("/api/values/current");
-    this.setState({ values: value.data });
+    this.setState({
+      values: value.data
+    });
   }
   async fetchIndexed() {
     const indexes = await axios.get("/api/values/all");
-    this.setState({ seeIndexes: indexes.data });
+    this.setState({
+      seeIndexes: indexes.data
+    });
+  }
+  renderSeenIndexes() {
+    return this.state.seeIndexes.map(({ number }) => number).join(", ");
+  }
+  renderValues() {
+    const entries = [];
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
+      );
+    }
+    return entries;
+  }
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    await axios.post("/api/values", {
+      index: this.state.index
+    });
+
+    this.setState({ index: "" });
+  };
+  render() {
+    return (
+      <div>
+        <form handleSubmit={this.handleSubmit}>
+          <label htmlFor="">Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={e => this.setState({ index: e.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+        <h3>Calculated values:</h3>
+        {this.renderValues()}
+      </div>
+    );
   }
 }
+
+export default Fib;
